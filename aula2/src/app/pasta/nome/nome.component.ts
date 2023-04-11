6d785c57bc31c28f24ef164fffaa4736ab0b4f51
unknown <fgabi2302@gmail.com>
2023-04-11 16:01:26 -0300

    Aula 2

---
+++ aula2/src/app/pasta/nome/nome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nome',
+  templateUrl: './nome.component.html',
+  styleUrls: ['./nome.component.css']
+})
+export class NomeComponent {
+
+}
